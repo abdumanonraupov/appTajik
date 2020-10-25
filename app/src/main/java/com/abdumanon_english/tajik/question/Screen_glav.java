d0d6ac72cd14e69439de4c47632bd233edc616f4
abdumanonraupov <73402817+abdumanonraupov@users.noreply.github.com>
2020-10-25 20:52:00 +0300

    update

--- app/src/main/java/com/abdumanon_english/tajik/question/Screen_glav.java
+++ app/src/main/java/com/abdumanon_english/tajik/question/Screen_glav.java
@@ -4,14 +4,20 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Screen_glav extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 10000;
+    TextView text_wel, text_wel_en;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_glav);
+        text_wel = (TextView)findViewById(R.id.text_wel);
+        text_wel_en = (TextView)findViewById(R.id.text_wel_en);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
